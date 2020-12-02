77bd4f978d3a0068aa37a4f6ff39d99398d28b7a
Janvi Shree Shrimal <59120284+Janvijain@users.noreply.github.com>
2020-12-03 01:18:02 +0530

    Add files via upload

---
+++ storage/framework/views/837c4bf088e6e2d200ce3884cfb51879d1a58c90.php
@@ -0,0 +1,54 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Persons Records</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="<?php echo e(route('persons.create')); ?>"> Create New person</a>
+            </div>
+        </div>
+    </div>
+   
+    <?php if($message = Session::get('success')): ?>
+        <div class="alert alert-success">
+            <p><?php echo e($message); ?></p>
+        </div>
+    <?php endif; ?>
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>emails</th>
+            <th width="280px">Action</th>
+        </tr>
+        <?php $__currentLoopData = $persons; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $person): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e(++$i); ?></td>
+            <td><?php echo e($person->name); ?></td>
+            <td><?php echo e($person->email); ?></td>
+            <td>
+                <form action="<?php echo e(route('persons.destroy',$person->id)); ?>" method="POST">
+   
+                    <a class="btn btn-info" href="<?php echo e(route('persons.show',$person->id)); ?>">Show</a>
+    
+                    <a class="btn btn-primary" href="<?php echo e(route('persons.edit',$person->id)); ?>">Edit</a>
+   
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?>
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+  
+    <?php echo $persons->links(); ?>
+
+      
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('persons.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp2\htdocs\laracrud\resources\views/persons/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d363cf0da52feaa53844f3b1983d62d9193f5aaf.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 6 CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+   
+</body>
+</html><?php /**PATH C:\xampp2\htdocs\laracrud\resources\views/persons/layout.blade.php ENDPATH**/ ?>
